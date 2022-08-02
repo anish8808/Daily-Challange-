e93c4f63d83549edc4c2880b45c0e4b3e44ac375
Anish Kashyap <anishkashyap11111@gmail.com>
2022-08-02 20:50:09 +0530

    2Aug of Code

---
+++ Elements_in_the_range.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	
+	
+	public:
+	bool check_elements(int arr[], int n, int A, int B)
+	{
+	    bool flag = false;
+		for(int i=A; i<=B; i++)
+		{
+		    int count =0 ;
+		    for(int j=0; j<=n-1; j++)
+		    {
+		        if(arr[j]==i)
+		         count++;
+		    }
+		    if(count==0)
+		      return false;
+		}
+		return true;
+	}
+
+
+		 
+
+};
+	
+
+//{ Driver Code Starts.
+
+
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+    	int n,A,B;
+		cin>>n>>A>>B;
+		int a[n];
+		for(int i=0;i<n;++i)
+			cin>>a[i];
+
+        
+
+        Solution ob;
+        if (ob.check_elements(a, n, A, B))
+			cout << "Yes";
+		else
+			cout << "No";
+	    
+        
+	    cout << "\n";
+	     
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+// } Driver Code Ends
